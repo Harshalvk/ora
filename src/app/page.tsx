35a4d2afcafafd77a3da524cfd06d6a28c3b9602
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-16 23:10:10 +0530

    ADD: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,13 @@ export default function Home() {
   return (
     <main>
       <Navbar />
+      <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">4
+        <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]">
+          <div className="flex flex-col items-center mt-24">
+            <h1 className="font-semibold text-6xl text-center first-line:tracking-tighter max-w-4xl bg-gradient-to-tl from-neutral-500 to-white text-transparent bg-clip-text">Automate Your Social Media, Elevate Your Brand</h1>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
