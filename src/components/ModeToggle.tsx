79888b294334f1d8190ae4301b276bc80979c8fb
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-17 18:11:39 +0530

    FIX: convert mode-toggle from slider -> dropdown

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,27 +1,40 @@
 "use client";
 
-import React from "react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import * as React from "react";
+import { Moon, MoonIcon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { Computer, Moon, Sun } from "lucide-react";
 
-const ModeToggle = () => {
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export function ModeToggle() {
   const { setTheme } = useTheme();
-  return (
-    <Tabs defaultValue="system" className="w-80 h-fit">
-      <TabsList>
-        <TabsTrigger value="light" onClick={() => setTheme("light")}>
-          <Sun height={16} width={16}/>
-        </TabsTrigger>
-        <TabsTrigger value="system" onClick={() => setTheme("dark")}>
-          <Computer height={16} width={16}/>
-        </TabsTrigger>
-        <TabsTrigger value="dark" onClick={() => setTheme("dark")}>
-          <Moon height={16} width={16}/>
-        </TabsTrigger>
-      </TabsList>
-    </Tabs>
-  );
-};
 
-export default ModeToggle;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
