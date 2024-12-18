f99c07787bd19d4d41e1867ee3028764a6c8c120
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-18 19:08:37 +0530

    FIX: for deployment

--- src/app/(main)/(pages)/dashboard/page.tsx
+++ src/app/(main)/(pages)/dashboard/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-type Props = {};
-
-const page = (props: Props) => {
+const page = () => {
   return <div className="flex flex-col gap-4 relative">
   <h1 className="text-4xl sticky top-0 z-10 p-6 bg-background/50 backdrop-blur-lg flex items-center border-b">
     Dashborad
