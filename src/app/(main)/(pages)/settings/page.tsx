85616f5ea3c9662e220b1014352985b79217e52c
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-21 21:34:17 +0530

    ADD: default values for Profile-form & setup ProfilePicture component

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,7 +1,16 @@
 import ProfileForm from "@/components/forms/ProfileForm";
+import ProfilePicture from "@/components/ProfilePicture";
 import React from "react";
+import { auth } from "../../../../../auth";
+import { redirect } from "next/navigation";
 
-const Settings = () => {
+const Settings = async () => {
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <h1 className="sticky top-0 z-10 flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
@@ -14,7 +23,11 @@ const Settings = () => {
             Add or update your information
           </p>
         </div>
-      <ProfileForm />
+        <ProfilePicture
+          profileUrl={session.user?.image!}
+          name={session?.user?.name!}
+        />
+        <ProfileForm user={session.user} />
       </div>
     </div>
   );
