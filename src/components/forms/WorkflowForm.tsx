9fdd1e60353c3eddc0afdc0f0bef598f515b94f7
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-22 16:44:34 +0530

    ADD: Workflow forms

---
+++ src/components/forms/WorkflowForm.tsx
@@ -0,0 +1,107 @@
+import { WorkflowFormSchema } from "@/lib/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+
+type Props = {
+  title?: string;
+  subTitle?: string;
+};
+
+const WorkflowForm = ({ title, subTitle }: Props) => {
+  const form = useForm<z.infer<typeof WorkflowFormSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(WorkflowFormSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+    },
+  });
+
+  const isLoading = form.formState.isLoading;
+  const router = useRouter();
+
+  const handleSubmit = () => {};
+
+  return (
+    <Card className="w-full max-w-[650px] border-none">
+      {title && subTitle && (
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{subTitle}</CardDescription>
+        </CardHeader>
+      )}
+      <CardContent>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="flex flex-col text-left gap-5"
+          >
+            <FormField
+              disabled={isLoading}
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Workflow name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Ex. IG automation" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              disabled={isLoading}
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Ex. When someone comment gives them a message"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button>
+              {isLoading ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Saving
+                </>
+              ) : (
+                "Save Settings"
+              )}
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default WorkflowForm;
