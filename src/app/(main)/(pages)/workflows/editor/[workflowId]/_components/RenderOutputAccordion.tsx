1cf78e6b313051062388b59fb882998d4fb873ad
Harshalvk <harshalvkhobragade@gmail.com>
2025-01-07 13:36:10 +0530

    feat(editor): add dynamic output configuration panel for workflow nodes

---
+++ src/app/(main)/(pages)/workflows/editor/[workflowId]/_components/RenderOutputAccordion.tsx
@@ -0,0 +1,32 @@
+import { ConnectionProviderProps } from "@/providers/ConnectionsProvider";
+import { EditorState } from "@/providers/EditorProvider";
+import { useOraStore } from "@/store";
+import React from "react";
+import ContentBasedOnTitle from "./ContentBasedOnTitle";
+
+type Props = {
+  state: EditorState;
+  nodeConnection: ConnectionProviderProps;
+};
+
+const RenderOutputAccordion = ({ state, nodeConnection }: Props) => {
+  const {
+    googleFile,
+    setGoogleFile,
+    selectedSlackChannels,
+    setSelectedSlackChannels,
+  } = useOraStore();
+
+  return (
+    <ContentBasedOnTitle
+      nodeConnection={nodeConnection}
+      newState={state}
+      file={googleFile}
+      setFile={setGoogleFile}
+      selectedSlackChannels={selectedSlackChannels}
+      setSelectedSlackChannels={setSelectedSlackChannels}
+    />
+  );
+};
+
+export default RenderOutputAccordion;
