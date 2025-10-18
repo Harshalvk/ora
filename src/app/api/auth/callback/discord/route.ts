899c5d55d3305dc0b1ba49b7ebb27b0f429f256a
Harshalvk <harshalvkhobragade@gmail.com>
2025-10-18 20:39:28 +0530

    fix: typo & change some api logic

--- src/app/api/auth/callback/discord/route.ts
+++ src/app/api/auth/callback/discord/route.ts
@@ -3,48 +3,56 @@ import { NextResponse, NextRequest } from "next/server";
 import url from "url";
 
 export async function GET(req: NextRequest) {
-  const code = req.nextUrl.searchParams.get("code");
-  if (code) {
-    const data = new url.URLSearchParams();
-    data.append("client_id", process.env.DISCORD_CLIENT_ID!);
-    data.append("client_secret", process.env.DISCORD_CLIENT_SECRET!);
-    data.append("grant_type", "authorization_code");
-    data.append(
-      "redirect_uri",
-      "https://localhost:3000/api/auth/callback/discord"
-    );
-    data.append("code", code.toString());
+  try {
+    console.log(req.url);
+    const code = req.nextUrl.searchParams.get("code");
+    
+    if (code) {
+      const data = new url.URLSearchParams();
+      data.append("client_id", process.env.DISCORD_CLIENT_ID!);
+      data.append("client_secret", process.env.DISCORD_CLIENT_SECRET!);
+      data.append("grant_type", "authorization_code");
+      data.append(
+        "redirect_uri",
+        "http://localhost:3000/api/auth/callback/discord"
+      );
+      data.append("code", code.toString());
 
-    const output = await axios.post(
-      "https://discord.com/api/oauth2/token",
-      data,
-      {
+      const output = await fetch("https://discord.com/api/oauth2/token", {
+        method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
         },
+        body: new URLSearchParams(data),
+      });
+
+      if (output.ok) {
+        const response = await output.json();
+        const access = response.access_token;
+        const UserGuilds: any = await axios.get(
+          `https://discord.com/api/users/@me/guilds`,
+          {
+            headers: {
+              Authorization: `Bearer ${access}`,
+            },
+          }
+        );
+
+        const UserGuild = UserGuilds.data.filter(
+          (guild: any) => guild.id == response.webhook.guild_id
+        );
+
+        return NextResponse.redirect(
+          `http://localhost:3000/connections?webhook_id=${response.webhook.id}&webhook_url=${response.webhook.url}&webhook_name=${response.webhook.name}&guild_id=${response.webhook.guild_id}&guild_name=${UserGuild[0].name}&channel_id=${response.webhook.channel_id}`
+        );
       }
-    );
-
-    if (output.data) {
-      const access = output.data.access_token;
-      const UserGuilds: any = await axios.get(
-        `https://discord.com/api/users/@me/guilds`,
-        {
-          headers: {
-            Authorization: `Bearer ${access}`,
-          },
-        }
-      );
-
-      const UserGuild = UserGuilds.data.filter(
-        (guild: any) => guild.id == output.data.webhook.guild_id
-      );
-
-      return NextResponse.redirect(
-        `https://localhost:3000/connections?webhook_id=${output.data.webhook.id}&webhook_url=${output.data.webhook.url}&webhook_name=${output.data.webhook.name}&guild_id=${output.data.webhook.guild_id}&guild_name=${UserGuild[0].name}&channel_id=${output.data.webhook.channel_id}`
-      );
     }
-
     return NextResponse.redirect("https://localhost:3000/connections");
+  } catch (error) {
+    console.error("Failed discord callback route: ", error);
+    return NextResponse.json(
+      { error: "Failed discord callback route" },
+      { status: 500 }
+    );
   }
 }
