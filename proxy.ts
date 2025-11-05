6c1d6cac0b1fbfb814493ec079d44de8de3170c2
Harshalvk <harshalvkhobragade@gmail.com>
2025-11-05 22:14:16 +0530

    fix(proxy): implement session validation and redirect logic in proxy function

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,20 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./auth";
 
 export default NextAuth(authConfig).auth;
 
+export async function proxy(req: NextRequest) {
+  const session = await auth();
+
+  if (!session) {
+    return NextResponse.redirect("/");
+  }
+
+  return NextResponse.redirect(req.nextUrl);
+}
+
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/dashboard", "/connections", "/workflows", "/settings"],
 };
