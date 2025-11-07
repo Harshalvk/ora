b6003623eec9ee1082ee3f289c39d7e0d89c463c
Harshalvk <harshalvkhobragade@gmail.com>
2025-11-07 19:24:22 +0530

    feat(ContentBasedOnTitle): integrate Google Drive file fetching and display functionality

--- src/app/(main)/(pages)/workflows/editor/[workflowId]/_components/ContentBasedOnTitle.tsx
+++ src/app/(main)/(pages)/workflows/editor/[workflowId]/_components/ContentBasedOnTitle.tsx
@@ -8,19 +8,22 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { onContentChange } from "@/lib/editor-utils";
-import { nodeMapper } from "@/lib/types";
+import { GoogleFileType, nodeMapper } from "@/lib/types";
 import { ConnectionProviderProps } from "@/providers/ConnectionsProvider";
 import { EditorState } from "@/providers/EditorProvider";
 import { Option } from "@/store";
 import GoogleFileDetails from "./GoogleFileDetails";
 import GoogleDriveFiles from "./GoogleDriveFiles";
 import ActionButton from "./ActionButton";
+import { useEffect } from "react";
+import axios from "axios";
+import { toast } from "sonner";
 
 type Props = {
   nodeConnection: ConnectionProviderProps;
   newState: EditorState;
-  file: any;
-  setFile: (file: any) => void;
+  files: GoogleFileType[];
+  setFiles: (files: GoogleFileType[]) => void;
   selectedSlackChannels: Option[];
   setSelectedSlackChannels: (value: Option[]) => void;
 };
@@ -28,13 +31,28 @@ type Props = {
 const ContentBasedOnTitle = ({
   nodeConnection,
   newState,
-  file,
-  setFile,
+  files,
+  setFiles,
   selectedSlackChannels,
   setSelectedSlackChannels,
 }: Props) => {
   const { selectedNode } = newState.editor;
   const title = selectedNode.data.title;
+
+  useEffect(() => {
+    const reqGoogle = async () => {
+      const res = await axios.get("/api/drive");
+      if (res) {
+        toast.message(JSON.stringify("File found"));
+        setFiles(res.data.data.files);
+      } else {
+        toast.error("Something went wrong");
+      }
+    };
+
+    reqGoogle();
+  }, []);
+
   //@ts-ignore
   const nodeConnectionType: any = nodeConnection[nodeMapper[title]];
   if (!nodeConnectionType) return <p>Not connected</p>;
@@ -76,17 +94,19 @@ const ContentBasedOnTitle = ({
               }
             />
           ) : null}
-          {JSON.stringify(file) !== "{}" && title !== "Google Drive" && (
+          {JSON.stringify(files) !== "[]" && title === "Google Drive" && (
             <Card className="w-full">
               <CardContent className="px-2 py-3">
                 <div className="flex flex-col gap-4">
                   <CardDescription>Drive File</CardDescription>
                   <div className="flex flex-wrap gap-2">
-                    <GoogleFileDetails
-                      nodeConnection={nodeConnection}
-                      title={title}
-                      gFile={file}
-                    />
+                    {files?.map((file) => (
+                      <GoogleFileDetails
+                        nodeConnection={nodeConnection}
+                        title={title}
+                        gFile={file}
+                      />
+                    ))}
                   </div>
                 </div>
               </CardContent>
